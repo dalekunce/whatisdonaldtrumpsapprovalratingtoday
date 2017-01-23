87d87024340ef712cbd0273aeac84912436358da
Dale Kunce <dale@normalhabit.com>
2017-01-23 11:00:37 -0800

    cleanup

--- js/main.js
+++ js/main.js
@@ -21,40 +21,4 @@ function gettrumped () {
   });
 }
 
-//d3 stuff
-var svg = d3.select("#speedometer")
-.append("svg:svg")
-.attr("width", 600)
-.attr("height", 600);
-
-
-var gauge = iopctrl.arcslider()
-.radius(120)
-.events(false)
-.indicator(iopctrl.defaultGaugeIndicator);
-gauge.axis().orient("in")
-.normalize(true)
-.ticks(4)
-.tickSubdivide(3)
-.tickSize(10, 8, 10)
-.tickPadding(5)
-.scale(d3.scale.linear()
-.domain([0, 100])
-.range([-3*Math.PI/4, 3*Math.PI/4]));
-
-var segDisplay = iopctrl.segdisplay()
-.width(80)
-.digitCount(6)
-.negative(false)
-.decimals(0);
-
-svg.append("g")
-.attr("class", "segdisplay")
-.attr("transform", "translate(130, 200)")
-.call(segDisplay);
-
-svg.append("g")
-.attr("class", "gauge")
-.call(gauge);
-
 gettrumped();
